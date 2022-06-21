8461c3e7f15c4de2a1c022835844556f057a9649
D4N13L360 <107592948+D4N13L360@users.noreply.github.com>
2022-06-20 20:03:33 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# clase-11
+# PROC11_trex_Etapa0_V4
---
+++ sketch.js
@@ -0,0 +1,24 @@
+
+var trex ,trex_running;
+function preload(){
+ trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
+groung = loadImage("ground2.png");
+}
+
+function setup(){
+  createCanvas(600,200)
+  
+  //crear sprite de Trex
+ trex = createSprite(50,160,20,50);
+ trex.addAnimation("running",trex_running);
+ ground1 = createSprite(200,180,400,20);
+ ground1.addImage  (groung);
+}
+
+function draw(){
+  background("white")
+ if (keyDown("space")) {
+  trex.velocityY=-10
+ } 
+drawSprites();
+}
